9e6bfcc9ab57566c4eaaa509be7e99203fab53f0
Daniel Enagu <daniel@cygnusspin.com>
2024-01-10 13:15:54 +0100

    Mobile Design: Hero section complete moving to About Section

--- portfolio-app/app/components/Header/Header.tsx
+++ portfolio-app/app/components/Header/Header.tsx
@@ -11,32 +11,56 @@ export default function Header() {
     id='header'
     className='app-header'
     >
-      {/* TODO: Make a SCSS component */}
-      <a className='app-header__brand' href='#'>
-        <span>Enagu</span>
-      </a>
+      <div className="nav-container">
+        {/* TODO: Make a SCSS component */}
+        <a className='app-header__brand' href='#'>
+          <span>Enagu</span>
+        </a>
 
-      <MobileMenu
-          isOpen={isOpen}
-          setIsOpen={setIsOpen}
-        />
-        <DesktopMenu />
+        <MobileMenu
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+          />
+          <DesktopMenu />
 
-      {/* Toggle menu button
-      TODO: Make the navicon a scss component
-      */}
-        <button
-          className={`button app-header__icon app-header__icon${isOpen ? '--hidden': ''}`}
-          type='button'
-          data-toggle='collapse'
-          data-target='#navbarNavDropdown'
-          aria-controls='navbarNavDropdown'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <span className='app-header__icon__navicon'></span>
-        </button>
+        {/* Toggle menu button
+        TODO: Make the navicon a scss component
+        */}
+          <button
+            className={`button app-header__icon app-header__icon${isOpen ? '--hidden': ''}`}
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarNavDropdown'
+            aria-controls='navbarNavDropdown'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <span className='app-header__icon__navicon'></span>
+          </button>
+      </div>
+      <section id='hero' className='app-header__hero'>
+        <h1 className='flex-heading'>Hello
+          <span className='round-shape'></span>
+        </h1>
+        <article className="intro">
+          <h2 className="intro__name flex-heading">
+            <span className='rectangle-shape'></span> I'm
+            <span className='text-primary'>Daniel Enagu</span>
+          </h2>
+          <p className="intro__text">
+            <span className='pre'>a </span>
+            <strong className='profession'>FRONTEND DEVELOPER </strong>
+          </p>
+          <p>
+            who creates superior quality websites which is accessible for everyday use
+          </p>
+          <a
+          href="#contact"
+          className='intro__button button'
+          >Contact Me</a>
+        </article>
+      </section>
     </header>
   )
 }
--- portfolio-app/app/page.tsx
+++ portfolio-app/app/page.tsx
@@ -9,10 +9,7 @@ export default function Home() {
     <div>
       <Header />
       <main>
-      <div>Hello World!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa beatae corporis minima quae omnis debitis error magni libero illo unde, sed ducimus totam ut voluptatem est nesciunt recusandae rerum nemo!
-      </div>
-    </main>
+      </main>
       <Footer />
     </div>
     </>
